20c898a63f9969326860822dd5f52766245d52d4
Nick Schudlo <nschudlo@gmail.com>
2025-11-05 22:13:43 -0500

    Clean up ball class

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,33 +1,58 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Ball : MonoBehaviour
-{
-    // The number of balls to be drawn per second
+/**
+ * Handles drawing the trail of balls behind the ball object
+ */
+public class Ball : MonoBehaviour {
+    /**
+     * The number of balls to be drawn per second
+     */
     private const float BALLS_PER_SECOND = 200f;
 
-    // The minimum time to wait before calling draw
-    // again. This doesn't prevent drawing dots when
-    // the ball collides with the walls.
+    /**
+     * The minimum time to wait before calling draw
+     * again. This doesn't prevent drawing dots when
+     * the ball collides with the walls.
+     */
     private const float TIME_BETWEEN_DRAWS = 0.05f;
 
+    /**
+     * Reference to the ball renderer
+     */
     private SpriteRenderer ballRenderer;
+
+    /**
+     * Reference to the physics ball object
+     */
     private Rigidbody2D ballRigidBody;
+
+    /**
+     * Reference to the sprite renderer for the background
+     */
     private SpriteRenderer paintRenderer;
 
-    // Queue of colors to cycle through
-    private Queue<Color> colorsQueue = new Queue<Color>(new []{ 
+    /**
+     * List of colors to cycle through
+     */
+    private readonly Color[] COLOURS = {
         Color.blue, Color.green, Color.yellow, Color.red, Color.magenta
-    });
+    };
+    private int colourIdx = 0;
 
-    // Position on the most recent draw
+    /**
+     * Position on the most recent draw
+     */
     private Vector2 previousDrawPos;
 
-    // Time that has elapsed since the most recent draw event
+    /**
+     * Time that has elapsed since the most recent draw event
+     */
     float elapsed = 0f;
 
-    void Awake()
-    {
+    /**
+     * Handle getting references to needed components.
+     */
+    void Awake() {
         paintRenderer = GameObject.FindGameObjectWithTag("Paint")
             .GetComponent<SpriteRenderer>();
         ballRenderer = GetComponent<SpriteRenderer>();
@@ -37,7 +62,7 @@ public class Ball : MonoBehaviour
     }
 
     /**
-     * Initialize the ball.
+     * Initialize the ball movement.
      * @param force
      */
     public void Init(Vector2 force) {
@@ -50,12 +75,15 @@ public class Ball : MonoBehaviour
      */
     private void FixedUpdate() {
         elapsed += Time.deltaTime;
-        if(elapsed < TIME_BETWEEN_DRAWS) {
-            return;
+        if (elapsed >= TIME_BETWEEN_DRAWS) {
+            DrawDots(transform.position);
         }
-        DrawDots(transform.position);
     }
 
+    /**
+     * Handles the case where the ball hits the edge, so the
+     * trail of balls needs to be drawn early.
+     */
     private void OnCollisionEnter2D(Collision2D collision) {
         elapsed += Time.deltaTime;
         // Draw the dots to the contact position. This
@@ -71,7 +99,7 @@ public class Ball : MonoBehaviour
     private void DrawDots(Vector3 targetPos) {
         float steps = elapsed * BALLS_PER_SECOND;
         Texture2D bgTexture = paintRenderer.sprite.texture;
-        for (float t=0; t < 1; t += (1f/steps)) {
+        for (float t = 0; t < 1; t += 1f / steps) {
             Vector2 pos = Vector2.Lerp(previousDrawPos, targetPos, t);
             DrawDot(bgTexture, pos);
         }
@@ -83,10 +111,12 @@ public class Ball : MonoBehaviour
 
     /**
      * Draw a dot at a position
+     * @param bgTexture
      * @param pos
      */
     private void DrawDot(Texture2D bgTexture, Vector2 pos) {
-        Color color = colorsQueue.Dequeue();
+        Color color = COLOURS[colourIdx++];
+        colourIdx = colourIdx % COLOURS.Length;
 
         // Get the ball position on the paint layer
         Vector2 ballPos = Camera.main.WorldToScreenPoint(pos);
@@ -96,15 +126,15 @@ public class Ball : MonoBehaviour
         Texture2D ballTexture = ballRenderer.sprite.texture;
         int startX = ballX - (ballTexture.width / 2);
         int startY = ballY - (ballTexture.height / 2);
-        for(int x=0; x<ballTexture.width; x++) {
-            for(int y=0; y<ballTexture.height; y++) {
+        for (int x = 0; x < ballTexture.width; x++) {
+            for (int y = 0; y < ballTexture.height; y++) {
                 int bgX = startX + x;
                 int bgY = startY + y;
                 // Don't draw outside of background texture
-                if(bgX < 0 || bgY < 0 || bgX >= bgTexture.width || bgY >= bgTexture.height) {
+                if (bgX < 0 || bgY < 0 || bgX >= bgTexture.width || bgY >= bgTexture.height) {
                     continue;
                 }
-                
+
                 // Ignore clear pixels
                 if (ballTexture.GetPixel(x, y).a == 0) {
                     continue;
@@ -113,6 +143,5 @@ public class Ball : MonoBehaviour
                 bgTexture.SetPixel(bgX, bgY, color);
             }
         }
-        colorsQueue.Enqueue(color);
     }
 }
